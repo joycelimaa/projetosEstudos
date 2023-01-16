95a09e583130a5703b47893a483f7a857f780737
Joyce Lima Avelino <101815855+joycelimaa@users.noreply.github.com>
2023-01-16 14:34:22 -0300

    Delete EstudosJsCssHtml directory

--- EstudosJsCssHtml/errorMessage/index.js
+++
@@ -1,5 +0,0 @@
-let erro = document.getElementById("error");
-
-function error(){
-    erro.textContent = "Something went wrong, please try again."
-}
--- EstudosJsCssHtml/errorMessage/study01.js
+++
@@ -1,8 +0,0 @@
-//const: variable that never change it´s value 
-//var: it´s a global variable
-//;:semicolon
-//"": quotation marks 
-// %: remainder
-
-var array = [["joy", 19], ["cachinhos, 20"]];
-console.log(array);
